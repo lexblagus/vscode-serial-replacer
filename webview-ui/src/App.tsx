d66ceefc18612eb10b3b6492d1571ba663c07cdc
Lex Blagus <lex@blag.us>
2025-05-09 00:59:01 -0300

    Ready for UI development

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,7 +1,12 @@
 import { vscode } from "./utilities/vscode";
 import "./App.css";
-// import "@vscode-elements/elements/dist/vscode-button/index.js";
-import { VscodeButton, VscodeCollapsible, VscodeIcon } from "@vscode-elements/react-elements";
+import {
+  VscodeButton,
+  VscodeCollapsible,
+  VscodeIcon,
+  VscodeScrollable,
+  VscodeTree,
+} from "@vscode-elements/react-elements";
 
 if (import.meta.env.DEV) {
   await import("@vscode-elements/webview-playground");
@@ -18,23 +23,17 @@ function App() {
   return (
     <main>
       <p>
-        <VscodeButton onClick={handleHowdyClick}>hello</VscodeButton>
+        <VscodeButton onClick={handleHowdyClick} icon="replace-all">hello</VscodeButton>
       </p>
 
-      <p>
-        <VscodeCollapsible title="Step 1">
-          <p>Suspendisse potenti. Maecenas eu egestas metus. Nulla eget placerat mi, et efficitur augue.</p>
-        </VscodeCollapsible>
-      </p>
-
-      <p>
-        Account icon:{' '}
-        <VscodeIcon name="account" />
-        <VscodeIcon name="account">account</VscodeIcon>
-        <br />
-        Account  span class:{' '}
-        <span className="codicon codicon-account"></span>
-      </p>
+      <VscodeCollapsible title="Step 1" description="First replacement">
+        <VscodeIcon name="refresh" action-icon slot="actions" aria-role="button"></VscodeIcon>
+        <VscodeIcon name="gear" action-icon aria-role="button" slot="decorations"></VscodeIcon>
+        <p>
+          Suspendisse potenti. Maecenas eu egestas metus. Nulla eget placerat mi, et efficitur
+          augue.
+        </p>
+      </VscodeCollapsible>
 
       {import.meta.env.DEV ? <vscode-dev-toolbar></vscode-dev-toolbar> : null}
     </main>
